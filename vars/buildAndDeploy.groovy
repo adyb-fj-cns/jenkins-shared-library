5ec9dd230da1ed63477f686e40b481f566d15f34
Ady Buxton <ady.buxton@fujitsu.com>
2020-10-13 22:50:48 +0100

    Updated

--- vars/buildAndDeploy.groovy
+++ vars/buildAndDeploy.groovy
@@ -1,6 +1,11 @@
 //vars//buildAndDeploy.groovy
 
-def call(){
+def call(body){
+    def config = [:];
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+    body()
+
     stages {
         stage("build"){
             steps {
