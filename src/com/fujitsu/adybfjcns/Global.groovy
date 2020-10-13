88043a136396c8199a60531afa945f9f67ac5c7a
Ady Buxton <ady.buxton@fujitsu.com>
2020-10-13 21:40:21 +0100

    Updated

---
+++ src/com/fujitsu/adybfjcns/Global.groovy
@@ -0,0 +1,7 @@
+// src/com/fujitsu/adybfjcns/Global.groovy
+
+package com.fujitsu.adybfjcns;
+
+class Global {
+    static String foo = 'bar'
+}
--- src/com/fujitsu/adybfjcns/Say.groovy
+++ src/com/fujitsu/adybfjcns/Say.groovy
@@ -2,8 +2,8 @@
 
 package com.fujitsu.adybfjcns;
 
-def say(String message) {
-    echo message
+def say(String message = 'World') {
+    echo "Hello ${message}"
 }
 
 return this
